d42e62ef04e5b3f2551224b588e1f53b526939a1
Nick Ulle <nick.ulle@gmail.com>
2015-04-16 11:37:28 -0700

    Add job script for timing2_O3.

---
+++ hw1/job3.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+# test job script. submit using "sbatch <script name>"
+#SBATCH -p development
+#SBATCH -n 1
+#SBATCH -o output/timing2.%j.out
+#SBATCH -e output/timing2.%j.err
+#SBATCH -t 00:05:00
+#SBATCH -A TG-ASC150014
+
+echo 'Running `timing2_O3 5000000`...'
+for i in {1..10}
+do
+    echo "Iteration $i:"
+    bin/timing2_O3 5000000
+done
+
+echo ""
+echo 'Running `timing2_O3 10000000`...'
+for i in {1..10}
+do
+    echo "Iteration $i:"
+    bin/timing2_O3 10000000
+done
+
+echo ""
+echo 'Running `timing2_O3 20000000`...'
+for i in {1..10}
+do
+    echo "Iteration $i:"
+    bin/timing2_O3 20000000
+done
+
