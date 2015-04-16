f10e1f136b207e975de85513bc70dfa73fc4080b
Nick Ulle <nick.ulle@gmail.com>
2015-04-16 10:22:44 -0700

    Fix job script for timing1.

--- hw1/job1.sh
+++ hw1/job1.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 # test job script. submit using "sbatch <script name>"
+#SBATCH -a 0-9
 #SBATCH -p development
-#SBATCH -n 10
+#SBATCH -n 1
 #SBATCH -o output/timing1.%j.out
 #SBATCH -e output/timing1.%j.err
 #SBATCH -t 00:05:00
