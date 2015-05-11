08e8fae5d2c360b9b58c4aa806441791cadd71f3
Nick Ulle <nick.ulle@gmail.com>
2015-05-10 22:10:21 -0700

    Set reasonable time limits for all jobs.

--- hw2/job1.sh
+++ hw2/job1.sh
@@ -4,9 +4,9 @@
 
 #SBATCH -p normal
 #SBATCH -n 1
-#SBATCH -o output/timing1.%j.out
-#SBATCH -e output/timing1.%j.err
-#SBATCH -t 02:00:00
+#SBATCH -o output/task1.%j.out
+#SBATCH -e output/task1.%j.err
+#SBATCH -t 05:00:00
 #SBATCH -A TG-ASC150014
 
 dims="100 200 500 1000 2000 5000"
--- hw2/job2.sh
+++ hw2/job2.sh
@@ -4,9 +4,9 @@
 
 #SBATCH -p normal
 #SBATCH -n 1
-#SBATCH -o output/timing2.%j.out
-#SBATCH -e output/timing2.%j.err
-#SBATCH -t 02:00:00
+#SBATCH -o output/task2.%j.out
+#SBATCH -e output/task2.%j.err
+#SBATCH -t 02:30:00
 #SBATCH -A TG-ASC150014
 
 dims="100 200 500 1000 2000 5000"
